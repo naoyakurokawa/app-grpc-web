c1b3f4a713d667970497e458a77ea0eb55187028
Naoya Kurokawa <kurokawa14@gmail.com>
2021-03-05 18:27:37 +0900

    aws-sdkを用いてファイルアップロード

--- client/components/UserRegisterForm.js
+++ client/components/UserRegisterForm.js
@@ -3,76 +3,60 @@ import {CreateUserRequest} from '../lib/hello_pb';
 import {HelloServiceClient} from '../lib/HelloServiceClientPb';
 import { BASEURL } from './const/form_data';
 import { BUCKET } from './const/form_data';
-import axios from 'axios';
-import * as minio from "minio";
-// import { useForm } from "react-hook-form";
+import AWS from 'aws-sdk';
 
 export const UserRegisterForm = () => {
-  // console.log('aaaaaaaaaaa',BASEURL)
   const [name, setName] = useState('');
   const [score, setScore] = useState('');
   const [profileImage, setProfileImage] = useState('');
   const [imageFile, setImage] = useState('');
   const [imageUploadUrl, setImageUploadUrl] = useState('');
 
-  //minio設定
-  // var Minio = require('minio')
-  const minioClient = new minio.Client({
-    endPoint: "play.min.io",
-    port: 9000,
-    useSSL: false,
-    accessKey: "minio",
-    secretKey: "minio123"
+  const s3  = new AWS.S3({
+    accessKeyId: 'minio' ,
+    secretAccessKey: 'minio123' ,
+    endpoint: 'http://127.0.0.1:9000' ,
+    s3ForcePathStyle: true, // needed with minio?
+    signatureVersion: 'v4'
   });
 
-  //バケット作成
-  const createBucket = (bucketName) => {
-    minioClient.makeBucket(bucketName, location, function(err) {
-      if(err) return console.log(err);
-
-      console.log('Bucket created successfully in ', location);
-      console.log('Created bucket name  => ', bucketName);
-    });
-  }
-
-  const metaData = {
-    'Content-Type': 'application/octet-stream',
-    'X-Amz-Meta-Testing': 1234,
-    'example': 5678
-  }
-
-
+  //画像を選択した際の処理
   const processImage = (event) => {
     const File = event.target.files[0];
     setImage(File);
     const imageUrl = URL.createObjectURL(File);
     setProfileImage(imageUrl);
-    console.log("qqqqqq",File);
     const uploadURL =  BASEURL + File.name;
     setImageUploadUrl(uploadURL);
-    console.log("aaaa",uploadURL);
   }
 
-  //フォームsubmit
+  //画像アップロードメソッド
+  const uploadImg = (file) => {
+    var fileName = file.name;
+    var params = {
+        Bucket: BUCKET,
+        Key: fileName,
+        ContentType: file.type,
+        Body: file
+      }
+    s3.putObject(params, function(err, data) {
+      if (err)
+        console.log(err)
+      else
+        console.log("Successfully uploaded data to testbucket/testobject");
+    });
+  }
+
+  //フォームsubmit時の処理
   const submitUserRegisterForm = async(event) => {
     event.preventDefault();
-    //バケット作成
-    // createBucket(BUCKET);
-    //画像をminioに保存、画像ファイル名の取得
-    console.log("aaaa",imageUploadUrl);
-    //minioへのアップロード
-    minioClient.fPutObject(BUCKET, imageFile.name, imageUploadUrl, metaData, (err, etag) => {
-        if (err)
-          return console.log(err);
-        console.log('File upload successfully!!');
-        console.log('Uploaded File Name => ', file);
-      });
-
+    //画像アップロード
+    uploadImg(imageFile);
     const request = new CreateUserRequest();
     request.setName(name);
     request.setScore(score);
-    //DBには画像のURLを保存(minio)
-    request.setPhotoUrl(imageUploadUrl);
+    //DBには画像のURLを保存
+    request.setPhotourl(imageUploadUrl);
     const client = new HelloServiceClient("http://localhost:8080");
     const response = await client.createUser(request, {});
   }
@@ -80,7 +64,6 @@ export const UserRegisterForm = () => {
     <div className="user-form-container">
       <h4>user-register-form</h4>
       <form onSubmit={submitUserRegisterForm}>
-      {/* onSubmit={event => this.ubmitUserRegisterForm(event)} */}
         <div>
           <p>名前</p>
           <input
--- client/components/const/form_data.js
+++ client/components/const/form_data.js
@@ -2,9 +2,9 @@ export const FORMDATA  = [
   'DEFAULT',
 ];
 
-export const STRAGEURL  = 'http://127.0.0.1:9000/';
+export const STRAGEURL  = 'http://127.0.0.1:9000';
 export const BUCKET  = 'grpc-web-test';
-export const BASEURL = STRAGEURL + BUCKET;
+export const BASEURL = STRAGEURL + '/' + BUCKET + '/';
 
 //テスト的にAPIのように、Languagesを取得する処理
 export const getFormData = () =>{
